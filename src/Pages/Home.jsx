7b1610606099d64a341e1b7e55281c66217eaa70
amuni5323 <156849494+amuni5323@users.noreply.github.com>
2025-01-10 09:25:31 +0300

    fix get all pages route

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
         }
     
     useEffect(()=>{
-        axios.get('https://backend-book-499o.onrender.com/books',{
+        axios.get('https://backend-book-499o.onrender.com/books/books',{
             headers: {
                 'Authorization': `Bearer ${usernameLocal}`
             }
